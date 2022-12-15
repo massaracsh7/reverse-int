516d1c91302aec03c2fc220c3e9ffdb0cdefe1e1
Your Name <you@example.com>
2022-12-15 20:12:56 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = Math.abs(n).toString().split('').reverse().join('');
+    let number = str.startsWith('0') ? parseInt(str.slice(1)) : parseInt(str);
+    return number;
 }
